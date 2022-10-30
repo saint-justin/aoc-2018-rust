c5d158c3e62655219534a61eba75fe6af828f866
Justin Vaughn <justin@jvaughn.org>
2022-10-30 09:49:06 -0600

    Refactoring day 2 p 1 solution module

--- src/day_02/mod.rs
+++ src/day_02/mod.rs
@@ -1,30 +1,35 @@
 pub mod solutions {
   use std::collections::HashMap;
 
+
   pub fn find_dups(input_vec: &Vec<&str>) {
+    let mut dups: (i32, i32) = (0, 0); // (doubles, triples)
     let mut map: HashMap<char, i32> = HashMap::new();
-    let mut multiples: (i32, i32) = (0, 0);
     
-    for id in input_vec {
-      for ch in id.chars() {
-        if map.contains_key(&ch) { map.insert(ch, *map.get(&ch).unwrap() + 1); } else { map.insert(ch, 1); }
+    for input in input_vec {
+      for ch in input.chars() {
+        if map.contains_key(&ch) { 
+          map.insert(ch, *map.get(&ch).unwrap() + 1); 
+        } else { 
+          map.insert(ch, 1); 
+        }
       }
 
       let mut copy_found: (bool, bool) = (false, false);
       for (_k, v) in &map {
-        if !copy_found.0 && v == &i32::from(2) {
-          multiples = (multiples.0, multiples.1 + 1);
-          copy_found = (true, copy_found.1);
-        } else if !copy_found.1 && v == &i32::from(3) {
-          multiples = (multiples.0 + 1, multiples.1);
-          copy_found = (copy_found.0, true);
+        if !copy_found.0 && *v == 2 {
+          dups.1 += 1;
+          copy_found.0 = true;
+        } else if !copy_found.1 && *v == 3 {
+          dups.0 += 1;
+          copy_found.1 = true;
         }
-        if copy_found == (true, true) { break; }
+        if copy_found == (true, true) { break; } // escape early if both have been found
       }
       map.clear();
     }
 
-    let checksum = multiples.0 * multiples.1;
+    let checksum = dups.0 * dups.1;
     println!("Checksum value: {checksum}");
   }
 }
